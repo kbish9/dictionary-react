5e5253edb9727397027bce149e582810619f1d84
kbish9 <103216809+kbish9@users.noreply.github.com>
2022-05-02 17:58:12 +0100

    added responsive elements

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,6 +5,8 @@ export default function Phonetic(props) {
   return (
     <div className="phonetic">
       <ReactAudioPlayer src={props.phonetic.audio} controls/>
+      <br className="d-none-sm"/>
+      <br className="d-none-sm"/>
       <span className="text">
          {props.phonetic.text}
       </span>
