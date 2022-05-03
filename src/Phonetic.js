03f1477a058688bbfb70d5afd25ebb0c75cb2523
kbish9 <103216809+kbish9@users.noreply.github.com>
2022-05-03 18:34:06 +0100

    update to filter out non working audio files

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
+  console.log(props.phonetic);
   return (
     <div className="phonetic">
       <ReactAudioPlayer src={props.phonetic.audio} controls/>
--- src/Results.js
+++ src/Results.js
@@ -11,13 +11,17 @@ export default function Result(props){
          <h2>
          {props.results.word}
       </h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-        return (
-          <div key={index}>
-            <Phonetic phonetic={phonetic} />
-          </div>
-        );
-      })}
+      {props.results.phonetics
+          .filter(function (phonetic) {
+            return phonetic.audio;
+          })
+          .map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
      </section>
            {props.results.meanings.map(function (meaning, index) {
         return (
